edb595699e0d985ad36f2a5fef5a1cc8f9738929
Shaun McFall <shaunm.mcfall@gmail.com>
2023-06-28 15:02:22 -0400

    Move to Wiggle Wizzard to make room for new Dugeon

---
+++ WiggleWizzard/Content/Images/Dungeon.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Dungeon" tilewidth="16" tileheight="16" tilecount="200" columns="10">
+ <image source="Dungeon.png" width="160" height="320"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="edge" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="100" wangid="0,0,2,0,2,0,2,0"/>
+   <wangtile tileid="101" wangid="2,0,0,0,2,0,2,0"/>
+   <wangtile tileid="102" wangid="0,0,2,0,2,0,0,0"/>
+   <wangtile tileid="103" wangid="0,0,2,0,0,0,2,0"/>
+   <wangtile tileid="104" wangid="0,0,0,0,2,0,2,0"/>
+   <wangtile tileid="105" wangid="0,0,1,0,1,0,1,0"/>
+   <wangtile tileid="106" wangid="1,0,0,0,1,0,1,0"/>
+   <wangtile tileid="107" wangid="0,0,1,0,1,0,0,0"/>
+   <wangtile tileid="108" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="109" wangid="0,0,0,0,1,0,1,0"/>
+   <wangtile tileid="110" wangid="2,0,2,0,2,0,0,0"/>
+   <wangtile tileid="111" wangid="2,0,2,0,0,0,2,0"/>
+   <wangtile tileid="112" wangid="2,0,2,0,0,0,0,0"/>
+   <wangtile tileid="113" wangid="0,0,0,0,0,0,2,0"/>
+   <wangtile tileid="114" wangid="2,0,0,0,2,0,0,0"/>
+   <wangtile tileid="115" wangid="1,0,1,0,1,0,0,0"/>
+   <wangtile tileid="116" wangid="1,0,1,0,0,0,1,0"/>
+   <wangtile tileid="117" wangid="1,0,1,0,0,0,0,0"/>
+   <wangtile tileid="118" wangid="0,0,0,0,0,0,1,0"/>
+   <wangtile tileid="119" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="120" wangid="2,0,0,0,0,0,0,0"/>
+   <wangtile tileid="121" wangid="0,0,0,0,2,0,0,0"/>
+   <wangtile tileid="122" wangid="0,0,2,0,0,0,0,0"/>
+   <wangtile tileid="123" wangid="0,0,2,0,0,0,2,0"/>
+   <wangtile tileid="124" wangid="2,0,0,0,0,0,2,0"/>
+   <wangtile tileid="125" wangid="1,0,0,0,0,0,0,0"/>
+   <wangtile tileid="126" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="127" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="128" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="129" wangid="1,0,0,0,0,0,1,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
